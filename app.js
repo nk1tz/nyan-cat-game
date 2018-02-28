eb37c021be1c53584178f53d4792dc8a02da2c0e
Nathaniel Kitzke <nkitzke@shutterstock.com>
2018-02-28 02:05:16 -0500

    attemps at smaller still nearing 1360 bytes

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ function Enemy(xPos, diff) {
     this.y = this.y + timeDiff * this.speed;
     shadowBlur(30);
     c.shadowColor = "#000";
-    c.fillStyle = 'red';
+    c.fillStyle = '#d01';
     c.fillRect(this.x, this.y, EW, EH);
   }
 }
@@ -77,26 +77,26 @@ let enemies = [],
     v:[0, 0, 0, 0],
     update: function() {
       let { x, y, v } = this;
-      let d = v.reduce((acc, z) => z+acc) > 1 ? 0.77 : 1;
+      let d = v.reduce((acc, z) => z+acc) > 1 ? 1 : 1;
       (v[1] && x < GW - PW)
-      ? this.x = x + (timeDiff*v[1]*PS * d)
+      ? this.x += (timeDiff*PS * d)
       : (v[3] && x > 0)
-      ? this.x = x - (timeDiff*v[3]*PS * d)
+      ? this.x -= (timeDiff*PS * d)
       : 0;
       (v[0] && y > 0 + 10)
-      ? this.y = y - (timeDiff*v[0]*PS * d)
+      ? this.y -= (timeDiff*PS * d)
       : (v[2] && y < GH - PH - 20)
-      ? this.y = y + (timeDiff*v[2]*PS * d)
+      ? this.y += (timeDiff*PS * d)
       : 0;
       shadowBlur(30);
       c.shadowColor = "#000";
-      c.fillStyle = 'green';
+      c.fillStyle = '#1cb';
       c.fillRect(x, y, PW, PH);
     }
   },
   drawBackground = () => {
     shadowBlur(0);
-    c.fillStyle = 'lightgrey';
+    c.fillStyle = '#ccc';
     c.fillRect(0, 0, GW, GH);
   },
   drawText = t => {
@@ -118,12 +118,6 @@ let enemies = [],
   ),
   start = () => {
     enemies = [];
-    player = {
-      ...player,
-      x: 5 * PW,
-      y: GH - PH - 10,
-      v:[0, 0, 0, 0],
-    };
     canStart = 0;
     score = 0;
     lastFrame = Date.now();
@@ -143,12 +137,12 @@ let enemies = [],
     updateTimeDiff();
     score += ro(timeDiff/10);
     drawBackground();
-    while (enemies.filter(e => !!e).length < ME) {addEnemy()};
+    while (enemies.length < ME) {addEnemy()};
     updateEnemies();
     lastFrame = Date.now();
     drawScore();
     if (isPlayerDead()) {
-      explosion = new Explosion({ x: player.x, y: player.y }, c);
+      explosion = new Explosion({ x: player.x, y: player.y });
       canStart = 1;
       explosionLoop()
     } else {
@@ -156,16 +150,13 @@ let enemies = [],
       requestAnimationFrame(gameLoop);
     }
   },
-  updateEnemies = () => enemies.forEach((e, id) => e.y > GH ? delete enemies[id] : e.update(timeDiff)),
-  addEnemy = () => enemies.push(new Enemy(Math.floor(r() * (GW - EW + 1)), score, c));
+  updateEnemies = () => enemies.forEach((e, id) => e.y > GH ? enemies.splice(id,1) : e.update(timeDiff)),
+  addEnemy = () => enemies.push(new Enemy(Math.floor(r() * (GW - EW + 1)), score));
 
 onkeydown = ({ keyCode: k }) => {
+  k == 13 && canStart && start();
   let dir = k==38? 0 : k==39? 1 : k==40? 2 : k==37? 3 : -1;
   dir > -1 ? player.v[dir] = 1 : 0;
-};
-
-onkeypress = ({ keyCode: k }) => {
-  k == 13 && canStart ? start() : 0;
 };
 
 onkeyup = ({ keyCode: k }) => {
